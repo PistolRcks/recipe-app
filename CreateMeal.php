5a7c9138ae1bd4b23cf8170bf21bf905e57c7e2e
PistolRcks <17933691+PistolRcks@users.noreply.github.com>
2022-11-30 21:16:26 -0600

    Basic outlining of `CreateMeal.php`
    Most of the basics are done now for CreateMeal.php, which is nice.
    The form still does not do anything but for now the skeleton is there, `<datalist>` shenanigans included.

--- CreateMeal.php
+++ CreateMeal.php
@@ -1,23 +1,67 @@
 <!doctype html>
 <html>
 	<head>
-		<title>Create Menu</title>
+		<title>Create Meal</title>
 	</head>
 	<body>
+		<h1>Create a Meal</h1>
+		<!-- TODO: Beautify this later -->
 		<form action="">
-			<input type='text' name='meal' list='meal' onmouseover='this.focus()'><br><br>
+			<label for="recipe">Select Recipes</label><br>
+			<input type='text' name='recipe' list='recipe' onmouseover='this.focus()' placeholder="Search recipes...">
 			<!-- Shamelessly steal -->
-            <datalist id='meal'>
+            <datalist id='recipe'>
 <?php
+// Fill dropdown with recipes
 $conn = new mysqli("mscsdb.uwstout.edu", "mealplanneruser8", "Spaghetti33?", "mealplanner8");
-$result = $conn->query("SELECT name FROM meal");
-$mealnames = "";
+$result = $conn->query("SELECT name, idRecipe FROM recipe");
+
+$recipenames = "";
 while ($row = $result->fetch_assoc()) {
-	$mealnames .= "<option label='" . $row["name"] . "' value='" . $row["names"] . "'>";
+	$recipenames .= "<option label='" . $row["name"] . "' value='" . $row["idRecipe"] . "'>";
 }
-echo $mealnames;
+
+echo $recipenames;
 ?>
-            </datalist>
+			</datalist>
+			<!-- TODO: Button should add selected idRecipe to the sql ADD ROW op 
+				and add the name to the list
+			-->
+			<input id="recipe_add" type="button" value="Add Recipe"><br><br>
+
+			<label for="ethnic">Ethnicity: </label>
+			<input type='text' name='ethnic' list='ethnic' onmouseover='this.focus()' placeholder="Search ethnicities, or make one...">
+			<datalist id="ethnic">
+<?php
+$result = $conn->query("SELECT ethnicGroup FROM meal");
+
+// Make sure there are no duplicates first
+$ethniclist = array();
+while ($row = $result->fetch_assoc()) {
+	// remove trailing whitespace
+	$group = preg_replace("/\s+$/", "", $row["ethnicGroup"]);
+	if (!in_array($group, $ethniclist)) {
+		$ethniclist[] = $group; // Weird how php does pushes
+	}
+}
+
+// *Then* add options
+$ethnicnames = "";
+foreach ($ethniclist as $ethnic) {
+	$ethnicnames .= "<option label='" . $ethnic . "' value='" . $ethnic . "'>";
+}
+
+echo $ethnicnames;
+?>
+			</datalist>
+			<h2>Selected Recipes:</h2>
+			<!-- TODO: Added recipes will go here
+				Also this header should probably be dynamically created
+			-->
+			<p id="selected_recipes">You haven't selected any recipes!</p><br>
+
+			<!-- TODO: Make modal -->
+			<input id="create" type="button" value="Create Meal" name="create">
 		</form>
 	</body>
 </html>
