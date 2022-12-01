93a4e76fee0ad1e4a2fe3c71d633ed5705841932
PistolRcks <17933691+PistolRcks@users.noreply.github.com>
2022-11-30 19:31:54 -0600

    Basic dropdown for recipe selection

--- CreateMeal.php
+++ CreateMeal.php
@@ -1,9 +1,23 @@
 <!doctype html>
 <html>
 	<head>
-		<title>Recipe App Elijah Southman</title>
+		<title>Create Menu</title>
 	</head>
 	<body>
-        Create meal
+		<form action="">
+			<input type='text' name='meal' list='meal' onmouseover='this.focus()'><br><br>
+			<!-- Shamelessly steal -->
+            <datalist id='meal'>
+<?php
+$conn = new mysqli("mscsdb.uwstout.edu", "mealplanneruser8", "Spaghetti33?", "mealplanner8");
+$result = $conn->query("SELECT name FROM meal");
+$mealnames = "";
+while ($row = $result->fetch_assoc()) {
+	$mealnames .= "<option label='" . $row["name"] . "' value='" . $row["names"] . "'>";
+}
+echo $mealnames;
+?>
+            </datalist>
+		</form>
 	</body>
 </html>
